2cf262c3d2c01ce0329613b6ef931fd3c2e0d950
Jerniya <111222668+Jerniya@users.noreply.github.com>
2023-01-13 16:27:09 +0530

    Add files via upload

--- wrk.js
+++ wrk.js
@@ -1,5 +1,5 @@
 objs=['rock','paper','sciss']
-var n=0
+var n=0,m=0;
 const random=(objs)=>{
   const index=Math.floor(Math.random()*objs.length)
    return(objs[index])
@@ -8,7 +8,8 @@ const random=(objs)=>{
 var executescis=()=>{
 var computer=random(objs)
 if(computer=='rock'){document.getElementById('computer').src="rock.jpg";
-   document.getElementById('display').value="YOU LOSS THE MATCH"}
+   document.getElementById('display').value="YOU LOSS THE MATCH"
+m++}
 
 else if(computer=='paper'){document.getElementById('computer').src="pape.jpg";
     document.getElementById('display').value="YOU WON THE MATCH"
@@ -28,6 +29,7 @@ var executerock=()=>{
     
     else if(computer=='paper'){document.getElementById('computer').src="pape.jpg";
         document.getElementById('display').value="YOU LOSS THE MATCH"
+        m++
     }
     else{document.getElementById('computer').src="scissors-neon-icons.jpg";
     document.getElementById('display').value="YOU WON THE MATCH"
@@ -50,9 +52,21 @@ var executerock=()=>{
             document.getElementById('display').value="OH WE ARE FRIENDS"
         }
         else{ document.getElementById('computer').src="scissors-neon-icons.jpg";
-        document.getElementById('display').value="YOU LOSS THE MATCH"}
+        document.getElementById('display').value="YOU LOSS THE MATCH"
+    m++}
         } 
 
 
 
+    var stop=()=>{
+    if(m>n)
+        alert("SORRY YOU MISSED IT BY "+(m-n)+ " RUNS" +" REFRESH THE PAGE TO PLAY AGAIN");
+
+    else if(n>m)
+        alert("HURRAY!!!YOU WON THE MATCH BY "+(n-m)+" RUNS" + " REFRESH THE PAGE TO PLAY AGAIN");
+    
+    else
+        alert("OHHH MATCH GONE TIE REFRESH THE PAGE TO PLAY AGAIN")
+    }
+           
         
